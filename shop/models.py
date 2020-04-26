aef106f98c0f986a64a37b9ab5e16b16029509c8
Pratap Neaupane <sereversion@gmail.com>
2020-04-26 21:43:55 +0545

    Product Model created and data stored

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    published_date = models.DateField()
+
