f3bf7cc5e18f9bdf153d3c8c29f9157704d1fc28
Pratap Neaupane <sereversion@gmail.com>
2020-04-26 21:04:17 +0545

    Added different shop categories

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name='ShopHome')
+    path("", views.index, name='ShopHome'),
+    path("about/", views.about, name='AboutUs'),
+    path("contact", views.contact, name='ContactUs'),
+    path("tracker/", views.tracker, name='Tracking Status'),
+    path("search/", views.search, name='Search'),
+    path("productview/", views.productView, name='product'),
+    path("checkout/", views.checkout, name='checkout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,3 +5,33 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+
+def about(request):
+    return HttpResponse("We are at about")
+    #return render(request, 'shop/about.html')
+
+
+def contact(request):
+    return HttpResponse("We are at contact")
+    #return render(request, 'shop/contact.html')
+
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+    #return render(request, 'shop/tracker.html')
+
+
+def search(request):
+    return HttpResponse("We are at search")
+    #return render(request, 'shop/search.html')
+
+
+def productView(request):
+    return HttpResponse("We are at productView")
+    #return render(request, 'shop/productview.html')
+
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
+    #return render(request, 'shop/checkout.html')
